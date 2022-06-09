bf7ae15b82eae50d494d68b3a2382d9fcea8cd9c
Maycon Godoy <mykooon@hotmail.com>
2022-06-09 16:06:10 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+/* let tamanho = document.querySelector('.header')
+
+let menu = document.getElementById('botaoHamburguer')
+
+function mudarMenu() {
+    
+    if(window.innerWidth < 770) {        
+        menu.className = 'menuResponsivo'
+    } else {
+        menu.className = 'menuResponsivo hidden'
+    }
+}
+
+window.addEventListener('resize', mudarMenu) */
+
+/*window.addEventListener('resize', Sua Função Aqui)
+window.addEventListener('load', sua função)
+if(window.innerWidth < 800) {
+
+}
+*/
+
+
+
+
+
+
+
+
+
