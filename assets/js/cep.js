eea5c78ce451b836df261ffb74002b95a2038f75
Adams Castillo <102859028+Adamscastillo@users.noreply.github.com>
2022-07-09 16:36:28 -0300

    mundaca cep

--- assets/js/cep.js
+++ assets/js/cep.js
@@ -1,17 +1,23 @@
-const { calcularPrecoPrazo } = require('correios-brasil')
+function consultaEndereco() {
+  let cep = document.querySelector('#cep').value
 
-let args = {
-  sCepOrigem: '81200100',
-  sCepDestino: '21770200',
-  nVlPeso: '1',
-  nCdFormato: '1',
-  nVlComprimento: '20',
-  nVlAltura: '20',
-  nVlLargura: '20',
-  nCdServico: ['04014', '04510'],
-  nVlDiametro: '0'
+  if (cep.length !== 8) {
+    alert('CEP Invalido')
+    return
+  }
+
+  let url = `https://viacep.com.br/ws/${cep}/json/`
+
+  fetch(url).then(function (response) {
+    response.json().then(mostrarEndereco)
+  })
 }
 
-calcularPrecoPrazo(args).then(response => {
-  console.log(response)
-})
+function mostrarEndereco(dados) {
+  let resultado = document.querySelector('#resultado')
+  if (dados.erro) {
+    resultado.innerHTML = 'Não é Possivel Localizar Endereço filho!!'
+  } else {
+    resultado.innerHTML = `<p> Cidade: ${dados.localidade} - ${dados.uf}</p>`
+  }
+}
